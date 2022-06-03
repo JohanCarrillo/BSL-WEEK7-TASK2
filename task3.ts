bc6b1cbf397139caea27ef4cadf723625a23269e
Algios <92694872+JohanCarrillo@users.noreply.github.com>
2022-06-02 22:24:46 -0500

    Added some generic methods to Shrek

---
+++ task3.ts
@@ -0,0 +1,63 @@
+abstract class Character {
+	abstract get name(): string;
+
+	introduce(): void {
+		console.log(`I am ${this.name}`);
+	}
+
+	abstract usePower(): void;
+	
+	abstract speak(): void;
+}
+
+class Shrek extends Character {
+	get name(): string {
+		return 'Shrek';
+	}
+
+	usePower(): void {
+		console.log(`${this.name} Fart and cook a rat`);
+	}
+	
+	speak(): void {
+		console.log('*Say something in ogre')
+	}
+
+	figth<Type extends {name: string}>(enemy: Type): void {
+		console.log(`${this.name} is fighting ${enemy.name}`);
+	}
+
+	eat<T extends string[]> (listOfFood: T): void {
+		console.log(`${this.name} is eating: ${[...listOfFood]}`);
+	}
+}
+
+class Donkey extends Character {
+	get name(): string {
+		return 'Donkey';
+	}
+
+	usePower(): void {
+		console.log(`${this.name} F*** a dragonette and sing`);
+	}
+	
+	speak(): void {
+		console.log('*Talk about friendship')
+	}
+}
+
+class LordFarquaad extends Character {
+
+	get name(): string {
+		return 'Lord Farquaad';
+	}
+
+	usePower(): void {
+		console.log(`${this.name} Spend money and be short`);
+	}
+	
+	speak(): void {
+		console.log('*Say something about how ugly Shrek is');
+	}
+
+}
